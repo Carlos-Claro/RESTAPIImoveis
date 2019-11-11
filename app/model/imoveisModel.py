8e86fe0097689d3a16cfa385fd02df69163743e9
Carlos Claro <carlosclaro79@gmail.com>
2019-11-11 15:03:02 -0300

    qtde de itens integra

--- app/model/imoveisModel.py
+++ app/model/imoveisModel.py
@@ -119,7 +119,7 @@ class imoveisModel(object):
         if 'filtro' in data:
             query['where'].append(data['filtro'])
         #if 'localhost' in sys.argv:
-        #    query['where'].append('imoveis.id_cidade in (1,2)')
+        #    query['where'].append('imoveis.id_cidade in (2)')
         query['group'] = 'imoveis.id'
         query['ordem'] = 'imoveis.id_cidade ASC, imoveis.data_atualizacao DESC'
         query['offset'] = 0
