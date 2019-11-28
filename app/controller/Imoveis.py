4e943e75de3cbd4d5966346f9faaeae79b8f309e
Carlos Claro <carlosclaro79@gmail.com>
2019-11-28 15:23:33 -0300

    Altera int

--- app/controller/Imoveis.py
+++ app/controller/Imoveis.py
@@ -205,7 +205,7 @@ class Imoveis(object):
         if 'id_empresa' in request.args :
             data['where'] = {'tem_foto': False, 'cidades_id': {'$in': [9730, 2, 10, 4, 5, 27,1]}, 'id_empresa': request.args['id_empresa']}
         else:
-            data['where'] = {'tem_foto': False, 'cidades_id': {'$in': [9730, 2, 10, 4, 5, 27, 1]}}
+            data['where'] = {'tem_foto': False, 'cidades_id': {'$in': [9730, 2, 10, 4, 5, 27,1]}}
             
         data['sort'] = {'data_update':0, 'cidades_id': 1, 'ordem':0}
         data['limit'] = int(limit)
