27f16ff3d0f96799d519accbef22006b6b19e88c
Carlos Claro <carlosclaro79@gmail.com>
2024-08-29 17:25:53 -0300

    add idempresa

--- app/controller/Imoveis.py
+++ app/controller/Imoveis.py
@@ -78,8 +78,8 @@ class Imoveis(object):
     def get_ativos(self):
         data = {}
         data['limit'] = request.args['limit']
-        if request.args['id_empresa']:
-            data['filtro'] = ['imoveis.id_empresa = ' + request.args['id_empresa']]
+        if 'id_empresa' in request.args:
+            data['filtro'] = 'imoveis.id_empresa = ' + request.args['id_empresa']
         value = self.imoveisModel.getItens_integra(data)
         if value['qtde'] == 0 :
             return False
